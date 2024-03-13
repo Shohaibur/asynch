eb145fb7f19b31f271f1cca095ed08b78f4e86f3
Shohaibur <shoaibrochi9@gmail.com>
2024-03-13 13:46:32 +0600

    add user task input

--- asyncTask.js
+++ asyncTask.js
@@ -1,29 +1,21 @@
-var tasks = [
-  {task: "Homework"},
-  {task: "Coding"},
-  {task: "Cooking"},
-  {task: "Workout"},
-  {task: "Reading books"}
-]
-
-var task_name = prompt("Please enter the option number for the task:\n1. Homework \n2. Coding \n3. Cooking \n4. Workout \n5. Reading books");
+var task_name = prompt("Please enter task");
 var take_time = prompt("Please enter time (seconds)")
 
-task_name = task_name-1;
+
 take_time = take_time * 1000;
+
+console.log("Initiating please wait 1 second");
 
 var task = (task_name ,call_result)=>{
   setTimeout(()=>{
-    console.log(`${tasks[task_name].task} Task Assigned, wait for completion`),
+    console.log(`${task_name} Task Assigned, wait for completion`),
   call_result()
   }, 1000 );  
 };
 
-
-
 var result =()=>{
-  setTimeout(()=>{console.log(`Task - ${tasks[task_name].task} performing `)
-  setTimeout(()=>{console.log(`${tasks[task_name].task} took ${take_time/1000} seconds to complete`)
+  setTimeout(()=>{console.log(`Task - ${task_name} performing`)
+  setTimeout(()=>{console.log(`${task_name} took ${take_time/1000} seconds to complete`)
 },take_time);
 },1000);
 
