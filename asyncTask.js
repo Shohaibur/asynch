c7b1f9698ff6b8e16826142e2cb197b35395abcf
Shohaibur <shoaibrochi9@gmail.com>
2024-04-16 23:46:37 +0600

    rename function

--- asyncTask.js
+++ asyncTask.js
@@ -6,24 +6,22 @@ var tasks = [
   {task: "Reading books" , Time : "2000"}
 ]
 
-var task_name = prompt("Please enter the option number for the task:\n0. Homework (Time: 2 sec)\n1. Coding (Time: 3000)\n2. Cooking (Time: 4000)\n3. Workout (Time: 5000)\n4. Reading books (Time: 2000)");
+var prompt_task = prompt("Please enter the option number for the task:\n0. Homework (Time: 2 sec)\n1. Coding (Time: 3000)\n2. Cooking (Time: 4000)\n3. Workout (Time: 5000)\n4. Reading books (Time: 2000)");
 
 
-
-var task = (task_name,call_result)=>{
+var task = (prompt_task,call_result)=>{
   setTimeout(()=>{
-    console.log(`${tasks[task_name].task} Task Assigned, wait for completion`),
+    console.log(`${tasks[prompt_task].task} Task Assigned, wait for completion`),
   call_result()
   }, 1000 );  
 };
 
 
-
 var result =()=>{
-  setTimeout(()=>{console.log(`Task - ${tasks[task_name].task} selected `)
-  setTimeout(()=>{console.log(`${tasks[task_name].task} took ${tasks[task_name].Time}  milliseconds to complete`)
-},tasks[task_name].Time);
+  setTimeout(()=>{console.log(`Task - ${tasks[prompt_task].task} selected `)
+  setTimeout(()=>{console.log(`${tasks[prompt_task].task} took ${tasks[prompt_task].Time}  milliseconds to complete`)
+},tasks[prompt_task].Time);
 },0);
 
 };
-task(task_name,result);
+task(prompt_task,result);
