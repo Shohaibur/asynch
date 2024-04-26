b0501eed68e2b1e225b19757e0f85d755e5ff507
Shohaibur <shoaibrochi9@gmail.com>
2024-04-26 20:44:09 +0600

    add error handling for prompt

--- asyncTask.js
+++ asyncTask.js
@@ -1,23 +1,25 @@
 var task_name = prompt("Please enter task");
 var prompt_time = prompt("Please enter time (seconds)")
 
+if (isNaN(task_name) == false || isNaN(prompt_time)==true) {
+  console.log("Invalid Input");
+} else {
 
-prompt_time = prompt_time * 1000;
+console.log("Initiating task please wait (1 second) ");
 
-console.log("Initiating please wait 1 second");
-
-var task = (task_name ,call_result)=>{
-  setTimeout(()=>{
-    console.log(`${task_name} Task Assigned, wait for completion`),
-  call_result()
-  }, 1000 );  
-};
-
-var result =()=>{
-  setTimeout(()=>{console.log(`Task - ${task_name} performing`)
-  setTimeout(()=>{console.log(`${task_name} took ${prompt_time/1000} seconds to complete`)
-},prompt_time);
+  var task = (task_name ,call_result)=>{
+    setTimeout(()=>{
+      console.log(`${task_name} Task Assigned, wait for completion`),
+    call_result()
+    }, 1000 );  
+  };
+  var result =()=>{
+    setTimeout(()=>{console.log(`Task - ${task_name} performing`)
+    setTimeout(()=>{console.log(`${task_name} took ${prompt_time} seconds to complete`)
+  },prompt_time*1000);
 },1000);
+  
+  };
+  task(task_name,result);
+}
 
-};
-task(task_name,result);
