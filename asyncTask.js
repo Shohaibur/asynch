1b006bb5b58b0794366ee9dca8696e0aa2ce7e91
Shohaibur <shoaibrochi9@gmail.com>
2024-04-28 03:25:58 +0600

    fin

--- asyncTask.js
+++ asyncTask.js
@@ -1,31 +1,23 @@
-var task_name = prompt("Please enter task");
-var prompt_time = prompt("Please enter time (seconds)")
+async function doTask() {
+  try {
+    var task = prompt("Input task name:");
+    var duration = prompt("Input duration:");
 
-
-
-
-if (task_name !='NaN' || prompt_time == 'NaN ') {
-  console.log("Invalid Input");
-} else {
-
-  prompt_time = prompt_time * 1000;
-
-console.log("Initiating please wait 1 second");
-
-  var task = (task_name ,call_result)=>{
-    setTimeout(()=>{
-      console.log(`${task_name} Task Assigned, wait for completion`),
-    call_result()
-    }, 1000 );  
-  };
-  
-  var result =()=>{
-    setTimeout(()=>{console.log(`Task - ${task_name} performing`)
-    setTimeout(()=>{console.log(`${task_name} took ${prompt_time/1000} seconds to complete`)
-  },prompt_time);
-  },1000);
-  
-  };
-  task(task_name,result);
+    if (isNaN(task) == false || isNaN(duration) == true) {
+      throw new Error("Either task or duration invalid");
+    }
+    else {
+      console.log(`Task ${task} assigned wait for completion`);
+      const myPromise = new Promise((resolve) => {
+        setTimeout(() => {
+          console.log(`Task ${task} completed in ${duration} seconds`);
+          resolve();
+        }, duration * 1000);
+      });
+    };
+    await myPromise;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
-
+doTask();
